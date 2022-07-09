1c1e153905f4034c6e8238db2e90d64d4d148b40
geo <tp53.geo@gmail.com>
2022-07-09 07:14:20 +0200

    ajout focusout sur ingredient selector

--- js/components/ingredientSelector.js
+++ js/components/ingredientSelector.js
@@ -10,7 +10,7 @@ function initIngredientSelector() {
     placeholder,
     elements: ingredients,
     colorId: 1,
-    onclickEvent: openIngredientList,
+    onclickEvent: displayIngredientList,
   });
   const ingredientSelectorDOM = ingredientSelectorModel.getSelectorDOM();
 
@@ -20,13 +20,26 @@ function initIngredientSelector() {
   );
 }
 
-function openIngredientList() {
+function displayIngredientList() {
   const ingredientList = document.getElementById("ingredients-list");
   const ingredientInput = document.getElementById("ingredients-input");
   const ingredientSelector = document.getElementById("ingredients-selector");
 
   ingredientInput.type = "text";
   ingredientInput.setAttribute("value", "");
+  ingredientInput.focus();
+  ingredientInput.addEventListener("focusout", closeIngredientList);
   ingredientList.classList.remove("visibility--hidden");
   ingredientSelector.style.flexBasis = "60%";
+}
+
+function closeIngredientList() {
+  const ingredientList = document.getElementById("ingredients-list");
+  const ingredientInput = document.getElementById("ingredients-input");
+  const ingredientSelector = document.getElementById("ingredients-selector");
+
+  ingredientInput.type = "button";
+  ingredientInput.setAttribute("value", "Ingrédients");
+  ingredientList.classList.add("visibility--hidden");
+  ingredientSelector.style.flexBasis = "12%";
 }
