ecdd872210cb8a08463422825f7ba45df26d91e3
geo <tp53.geo@gmail.com>
2022-07-16 04:51:34 +0200

    creation de la recherche par mots clés en paradigme fonctionnel

--- js/utils/searchUtils.js
+++ js/utils/searchUtils.js
@@ -1,4 +1,8 @@
 function checkTagsMatchUstensils(recipe, tags) {
+  if (!tags.length) {
+    return true;
+  }
+
   const recipeUstensils = recipe.ustensils.map((ustensil) =>
     capitalizeFirstLetter(ustensil)
   );
@@ -6,6 +10,9 @@ function checkTagsMatchUstensils(recipe, tags) {
 }
 
 function checkTagsMatchIngredients(recipe, tags) {
+  if (!tags.length) {
+    return true;
+  }
   const recipeIngredients = recipe.ingredients.map((ingredient) =>
     capitalizeFirstLetter(ingredient.ingredient)
   );
@@ -13,55 +20,53 @@ function checkTagsMatchIngredients(recipe, tags) {
 }
 
 function checkTagMatchAppliance(recipe, tag) {
+  if (!tag) {
+    return true;
+  }
   return capitalizeFirstLetter(recipe.appliance) == tag;
 }
-function searchRecipeByTags(tags) {
-  let recipes = Api.getAllRecipes();
 
-  recipesResults = recipes.filter((recipe) => tags.forEach());
+function checkKeywordsMatchRecipeInfos(recipe, keywords) {
+  keywords = formatKeywords(keywords);
+  let recipeInfos = `${recipe.name} ${recipe.description}`;
+  recipe.ingredients.map((ingredient) => {
+    recipeInfos += ingredient + " ";
+  });
+  recipeInfos = replaceAccents(recipeInfos).toLowerCase();
+
+  return recipeInfos.includes(keywords);
 }
 
-// let recipe = {
-//   id: 4,
-//   name: "Salade de riz",
-//   servings: 4,
-//   ingredients: [
-//     {
-//       ingredient: "Riz blanc",
-//       quantity: 500,
-//       unit: "grammes",
-//     },
-//     {
-//       ingredient: "Thon en miettes",
-//       quantity: 200,
-//       unit: "grammes",
-//     },
-//     {
-//       ingredient: "Tomate",
-//       quantity: 2,
-//     },
-//     {
-//       ingredient: "Oeuf dur",
-//       quantity: 2,
-//     },
-//     {
-//       ingredient: "Maïs",
-//       quantity: 300,
-//       unit: "grammes",
-//     },
-//     {
-//       ingredient: "Vinaigrette",
-//       quantity: 5,
-//       unit: "cl",
-//     },
-//   ],
-//   time: 50,
-//   description:
-//     "Faire cuire le riz. Une fois le riz cuit, le laisser refroidir. Couper les oeufs dur en quarts ou en lamelle au choix, coupez le tomates en dés, ajouter au riz les oeufs, les tomates, le poisson, le maïs et la vinaigrette. Ajouter au gout de chacun des corniches, olives etc..",
-//   appliance: "cuiseur de riz",
-//   ustensils: ["saladier", "passoire", "Cuillère à soupe", "Verres"],
-// };
+function searchRecipesByTags(recipes, tags) {
+  let recipesResults = recipes.filter(
+    (recipe) =>
+      checkTagMatchAppliance(recipe, tags.appliance) &&
+      checkTagsMatchIngredients(recipe, tags.ingredients) &&
+      checkTagsMatchUstensils(recipe, tags.ustensils)
+  );
 
-// let ingredientsTags = ["Riz blanc", "Tomate"];
-// let ustensilsTags = ["Cuillère à soupe", "Verres"];
-// let applianceTag = "Cuiseur de riz";
+  return recipesResults;
+}
+
+function searchRecipesByKeywords(recipes, keywords) {
+  let recipesResults = recipes.filter((recipe) =>
+    checkKeywordsMatchRecipeInfos(recipe, keywords)
+  );
+
+  return recipesResults;
+}
+
+function searchRecipes(keywords, tags) {
+  let recipesResults = searchRecipesByTags(Api.getAllRecipes(), tags);
+  recipesResults = searchRecipesByKeywords(recipesResults, keywords);
+
+  return recipesResults;
+}
+
+function searchElementsByKeywords(keywords, dataset) {
+  let keywordsFormatted = formatKeywords(keywords);
+
+  return dataset.filter((element) =>
+    replaceAccents(element).includes(keywordsFormatted)
+  );
+}
